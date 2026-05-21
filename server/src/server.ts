9c2e19cf6ddb032a343afebc36676b67730d1e42
Yehor <chaliyegor00@gmail.com>
2026-05-21 18:29:58 +0300

    wire up the auth router

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import { notFoundMiddleware } from './middleware/not-found.js'
 import { errorHandlerMiddleware } from './middleware/error-handler.js'
 import connectDB from './db/connect.js'
+import authRouter from './routes/auth.js'
 const app = express()
 
 const port = process.env.PORT || 5000
@@ -12,6 +13,8 @@ if (!uri) {
 }
 
 app.use(express.json())
+
+app.use('/api/v1/auth', authRouter)
 
 app.get('/', (req, res) => {
 	res.send('pinged')
