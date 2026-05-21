78846962cc93073a0af45253c29adaa77d0dcf90
Yehor <chaliyegor00@gmail.com>
2026-05-21 17:21:57 +0300

    use error handler and not found middlewares in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
+import { notFoundMiddleware } from './middleware/not-found.js'
+import { errorHandlerMiddleware } from './middleware/error-handler.js'
 const app = express()
 
 const port = process.env.PORT || 5000
@@ -13,6 +15,9 @@ app.use(express.json())
 app.get('/', (req, res) => {
 	res.send('pinged')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`)
