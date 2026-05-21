81cfe6ceb8e00dbb46a723a27f08f5fec4752914
Yehor <chaliyegor00@gmail.com>
2026-05-21 21:57:18 +0300

    create authorization middleware which protects routes from being misused by wrong people

---
+++ server/src/middleware/authorization.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express'
+import { JwtPayload } from 'jsonwebtoken'
+import { UnauthenticatedError } from '../errors/unauthenticated.js'
+import jwt from 'jsonwebtoken'
+
+interface AuthPayload extends JwtPayload {
+	userID: string
+	name: string
+}
+
+export const authMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const authHeader = req.headers.authorization
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		return next(new UnauthenticatedError('User is not authorized'))
+	}
+	const token = authHeader.split(' ')[1]
+	const jwtSecret = process.env.JWT_SECRET
+	if (!jwtSecret) {
+		throw new Error('jwt secret is not defined')
+	}
+	try {
+		const payload = jwt.verify(token, jwtSecret) as AuthPayload
+		req.user = { userID: payload.userID, name: payload.name }
+		next()
+	} catch (error) {
+		return next(new UnauthenticatedError('Unauthorized'))
+	}
+}
