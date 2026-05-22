283198595ef3f116a7575cfc10377692d6910131
Yehor <chaliyegor00@gmail.com>
2026-05-22 19:12:43 +0300

    create constants folder with form types for signing in and out

---
+++ client/src/constants/formType.ts
@@ -0,0 +1,10 @@
+export const signUpFormInputs = [
+	'First Name',
+	'Last Name',
+	'Username',
+	'Email',
+	'Password',
+	'Confirm Password',
+]
+
+export const signInFormInputs = ['Username', 'Password']
