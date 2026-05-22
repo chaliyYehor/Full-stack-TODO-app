ebb877470c3cb0a1ab8826495d556a31d69f4c67
Yehor <chaliyegor00@gmail.com>
2026-05-22 18:19:20 +0300

    create Input component for reg form

---
+++ client/src/components/auth/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input() {
+	return (
+		<div className="inputWrapper">
+			<img src="/auth/formIcons/username.png" alt="icon" />
+			<input type="text" placeholder='Enter Username'/>
+		</div>
+	)
+}
