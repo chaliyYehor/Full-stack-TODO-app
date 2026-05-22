8ace292af0694932231fb506987237f4ff6810ea
Yehor <chaliyegor00@gmail.com>
2026-05-22 19:08:11 +0300

    create final markup for inputs based on input type

--- client/src/components/auth/AuthForm.tsx
+++ client/src/components/auth/AuthForm.tsx
@@ -1,7 +1,14 @@
+import Input from './Input'
+
 export default function AuthForm() {
 	return (
 		<form className='w-full'>
-			<input type="text" placeholder='asfasfasf'/>
+			<Input inpPlaceholder='First Name' />
+			<Input inpPlaceholder='Last Name' />
+			<Input inpPlaceholder='Username' />
+			<Input inpPlaceholder='Email' />
+			<Input inpPlaceholder='Password' />
+			<Input inpPlaceholder='Confirm Password' />
 		</form>
 	)
 }
--- client/src/components/auth/Input.tsx
+++ client/src/components/auth/Input.tsx
@@ -1,8 +1,59 @@
-export default function Input() {
+import { Eye, EyeOff, X } from 'lucide-react'
+import { useState } from 'react'
+
+interface Props {
+	inpPlaceholder:
+		| 'Username'
+		| 'Email'
+		| 'Password'
+		| 'Confirm Password'
+		| 'First Name'
+		| 'Last Name'
+}
+export default function Input({ inpPlaceholder }: Props) {
+	const [input, setInput] = useState('')
+	const [showPassword, setShowPassword] = useState(false)
+
+	let inpType = null
+	if (inpPlaceholder.startsWith('Confirm') || inpPlaceholder === 'Password') {
+		inpType = 'password'
+	} else if (inpPlaceholder === 'Email') {
+		inpType = 'email'
+	} else {
+		inpType = 'text'
+	}
+	const isPasswordInput = inpType === 'password'
+	const inputType = isPasswordInput && showPassword ? 'text' : inpType
+
 	return (
-		<div className="inputWrapper">
-			<img src="/auth/formIcons/username.png" alt="icon" />
-			<input type="text" placeholder='Enter Username'/>
+		<div className='inputWrapper select-none relative flex justify-center items-center gap-2 border-2 border-[#565454] rounded-lg py-2 px-1 mb-5.5 mt-5'>
+			<img className='w-15%' src='/auth/formIcons/username.png' alt='icon' />
+			<input
+				className=' w-full outline-none'
+				type={inputType}
+				value={input}
+				onChange={e => {
+					setInput(e.target.value)
+				}}
+				placeholder={
+					inpPlaceholder.startsWith('Confirm')
+						? inpPlaceholder
+						: `Enter ${inpPlaceholder}`
+				}
+			/>
+			{input.length > 0 && <X onClick={() => setInput('')} className='cursor-pointer' />}
+			{isPasswordInput && !showPassword && input.length > 0 && (
+				<Eye
+					onClick={() => setShowPassword(true)}
+					className='absolute -right-8'
+				/>
+			)}
+			{isPasswordInput && showPassword && input.length > 0 && (
+				<EyeOff
+					onClick={() => setShowPassword(false)}
+					className='absolute -right-8'
+				/>
+			)}
 		</div>
 	)
 }
