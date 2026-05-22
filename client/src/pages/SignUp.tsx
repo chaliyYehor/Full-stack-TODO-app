6a7cb52d933542e7d739c014c9de46a8245ecfe9
Yehor <chaliyegor00@gmail.com>
2026-05-22 17:26:33 +0300

    download images for registration section, create signup page and create folder with components for auth

---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,9 @@
+import Authentication from '../components/auth/Authentication'
+
+export default function SignUp() {
+	return (
+		<div className="signUpWrapper bg-[#FF6767] min-h-dvh w-full">
+			<Authentication />
+		</div>
+	)
+}
