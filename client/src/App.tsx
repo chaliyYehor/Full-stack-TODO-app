d6ea80288e75bc9f5cc5757db93edbf073ebc05b
Yehor <chaliyegor00@gmail.com>
2026-05-22 17:06:01 +0300

    setup a router with main page and signup page with an error page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Main from './pages/Main'
+import NotFound from './pages/NotFound'
+import SignUp from './pages/SignUp'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Main />,
+		errorElement: <NotFound />,
+	},
+	{
+		path: '/register',
+		element: <SignUp />,
+	},
+])
+
 function App() {
-	return <></>
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	)
 }
 
 export default App
