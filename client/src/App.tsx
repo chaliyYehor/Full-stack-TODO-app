4f878bfbbe90e754493ec5ebfc147a425c040a2a
Yehor <chaliyegor00@gmail.com>
2026-05-24 15:14:16 +0300

    create a separate page for signing in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Main from './pages/Main'
 import NotFound from './pages/NotFound'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 const router = createBrowserRouter([
 	{
@@ -10,8 +11,12 @@ const router = createBrowserRouter([
 		errorElement: <NotFound />,
 	},
 	{
-		path: '/register',
+		path: '/signUp',
 		element: <SignUp />,
+	},
+	{
+		path: '/signIn',
+		element: <SignIn />,
 	},
 ])
 
---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,9 @@
+import Authentication from '../components/auth/Authentication'
+
+export default function SignUp() {
+	return (
+		<div className='signInUpWrapper bg-[#FF6767] min-h-dvh w-dvw py-32.5 px-25 overflow-hidden'>
+			<Authentication authType='SignIn' />
+		</div>
+	)
+}
