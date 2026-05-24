359da528f42f8f14a2da1208917e8f608d042685
Yehor <chaliyegor00@gmail.com>
2026-05-24 22:11:03 +0300

    replace regular btn with Link component to let users redirect themselves to the home page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 function NotFound() {
 	return (
 		<div className='min-h-dvh w-full bg-[#FB8133] flex justify-center items-center p-4 sm:p-8 lg:p-15 overflow-auto'>
@@ -6,9 +8,9 @@ function NotFound() {
 					<img className='w-full max-w-140' src='/notFound.png' alt='404' />
 					<div className='info flex flex-col justify-end items-center pb-22.5 gap-15'>
 						<div className=' err-text uppercase'>OPPS! Page not found</div>
-						<button className='px-10.25 py-3 backHome rounded-4xl uppercase transition cursor-pointer bg-[#FB8133] hover:bg-[#f66810] text-white  text-[24px] shadow-2xl'>
+						<Link to={'/'} className='px-10.25 py-3 backHome rounded-4xl uppercase transition cursor-pointer bg-[#FB8133] hover:bg-[#f66810] text-white  text-[24px] shadow-2xl'>
 							Back to home
-						</button>
+						</Link>
 					</div>
 				</div>
 			</div>
