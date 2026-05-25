f0171a019743ddee4e075e959ee0474e8c0b7846
Yehor <chaliyegor00@gmail.com>
2026-05-25 19:26:29 +0300

    create an interceptor to set a token

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,5 +2,16 @@ import axios from 'axios'
 
 export const api = axios.create({
 	baseURL: 'http://localhost:5000',
-	withCredentials: true
+	withCredentials: true,
+})
+
+api.interceptors.request.use(config => {
+	const storedToken = localStorage.getItem('token')
+	const token = storedToken ? JSON.parse(storedToken) : null
+
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`
+	}
+
+	return config
 })
