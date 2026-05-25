957ee0c9a39e62185de534bdfaa9ef6b18f5d791
Yehor <chaliyegor00@gmail.com>
2026-05-25 19:26:39 +0300

    move token saving to hooks

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,12 +1,16 @@
 import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import { loginUser } from '../api/auth.api'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 export const useLogin = () => {
 	const navigate = useNavigate()
+	const [_, saveToken] = useLocalStorage('token', '')
+
 	return useMutation({
 		mutationFn: loginUser,
-		onSuccess: () => {
+		onSuccess: (data) => {
+			saveToken(data.token)
 			navigate('/')
 		},
 	})
--- client/src/hooks/useRegister.ts
+++ client/src/hooks/useRegister.ts
@@ -1,14 +1,17 @@
 import { useMutation } from '@tanstack/react-query'
 import { registerUser } from '../api/auth.api'
 import { useNavigate } from 'react-router-dom'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 export const useRegister = () => {
 	const navigate = useNavigate()
+	const [_, saveToken] = useLocalStorage('token', '')
 
 	return useMutation({
 		mutationFn: registerUser,
-		onSuccess: () => {
+		onSuccess: data => {
+			saveToken(data.token)
 			navigate('/')
-		}
+		},
 	})
 }
