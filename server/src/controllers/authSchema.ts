795f60a0e65963be12afab07093c32e7d5fe0dd1
Yehor <chaliyegor00@gmail.com>
2026-05-25 17:43:34 +0300

    add lastName firstName to the validation schema and redirect user after successful registration

--- server/src/controllers/authSchema.ts
+++ server/src/controllers/authSchema.ts
@@ -1,14 +1,16 @@
 import z from 'zod'
 
 export const reqBodySchema = z.object({
+	firstName: z.string().min(2, 'Please provide a valid First Name'),
+	lastName: z.string().min(3, 'Please provide a valid Last Name'),
 	username: z.string().min(3).max(50),
 	email: z.email(),
-	password: z.string().min(6),
+	password: z.string().min(6)
 })
 
 export const loginReqBodySchema = z.object({
 	email: z.email(),
-	password: z.string().min(6)
+	password: z.string().min(6),
 })
 
 export type RegisterReqBody = z.infer<typeof reqBodySchema>
