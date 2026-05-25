ad6f158e2474c9021f1b4bae64359b6f801c184a
Yehor <chaliyegor00@gmail.com>
2026-05-25 16:59:22 +0300

    change allowed password length

--- client/src/schemas/authFormSchema.ts
+++ client/src/schemas/authFormSchema.ts
@@ -6,8 +6,8 @@ export const registerSchema = z
 		lastName: z.string().min(3, 'Please provide a valid Last Name'),
 		username: z.string().min(4, 'Username is too short'),
 		email: z.email(),
-		password: z.string().min(5, 'Password is too short'),
-		confirmPassword: z.string().min(5, 'Password is too short'),
+		password: z.string().min(6, 'Password is too short'),
+		confirmPassword: z.string().min(6, 'Password is too short'),
 	})
 	.refine(data => data.password === data.confirmPassword, {
 		message: 'Passwords do not match',
@@ -18,7 +18,7 @@ export type RegisterType = z.infer<typeof registerSchema>
 
 export const loginSchema = z.object({
 	username: z.string().min(4, 'Username is too short'),
-	password: z.string().min(5, 'Password is too short'),
+	password: z.string().min(6, 'Password is too short'),
 })
 
 export type LoginType = z.infer<typeof loginSchema>
