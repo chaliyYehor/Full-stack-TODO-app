5edd5441f3798b37fd9466f936fb6019b3c13742
Yehor <chaliyegor00@gmail.com>
2026-05-25 17:02:12 +0300

    create queryOptions for registration mutation

---
+++ client/src/hooks/useRegister.ts
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query'
+import { registerUser } from '../api/auth.api'
+
+export const useRegister = () => {
+	return useMutation({
+		mutationFn: registerUser,
+	})
+}
