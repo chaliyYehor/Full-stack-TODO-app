5ad139b619a8b1c5644d1d4e2c8345a659b1bbe6
Yehor <chaliyegor00@gmail.com>
2026-06-01 17:06:30 +0300

    download icons for task completed and taskStatus comps

---
+++ client/src/components/dashboard/CompletedTask.tsx
@@ -0,0 +1,5 @@
+export default function CompletedTask() {
+	return (
+		<div>CompletedTask</div>
+	)
+}
---
+++ client/src/components/dashboard/TaskStatus.tsx
@@ -0,0 +1,7 @@
+export default function TaskStatus() {
+	return (
+		<div className="taskStatusWrapper h-40% w-full">
+
+		</div>
+	)
+}
