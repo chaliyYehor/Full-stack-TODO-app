583a2d42f223af5c795f878fe15a0f54054230d8
Yehor <chaliyegor00@gmail.com>
2026-06-05 15:19:48 +0300

    move Dashboard component to an appropriate folder

---
+++ client/src/components/main/Dashboard.tsx
@@ -0,0 +1,22 @@
+import CompletedTask from '../dashboard/CompletedTask'
+import TaskStatus from '../dashboard/TaskStatus'
+import ToDoSection from '../dashboard/ToDoSection'
+
+export default function Dashboard() {
+	return (
+		<div
+			className='dashboard-wrapper w-full
+		 h-[calc(100dvh-14rem)] mt-15 ml-19'
+		>
+			<h1 className='inline-block text-4xl'>Welcome back, Sundar 👋</h1>
+
+			<div className='dashboard-info-wrapper p-4 border-[#A1A3AB] border-2 gap-6 grid grid-cols-2 h-full min-h-0 overflow-hidden'>
+				<ToDoSection />
+				<div className='w-full h-full min-h-0 grid grid-rows-[2fr_3fr] gap-5'>
+					<TaskStatus />
+					<CompletedTask />
+				</div>
+			</div>
+		</div>
+	)
+}
