cdaee8024c2e627ec36916aec83bf32ee6a72f80
Yehor <chaliyegor00@gmail.com>
2026-06-05 15:15:57 +0300

    create TaskCreationForm comp and add a bit of markup for the task creating window

--- client/src/components/dashboard/taskCreation/CreateTask.tsx
+++ client/src/components/dashboard/taskCreation/CreateTask.tsx
@@ -5,11 +5,20 @@ type Props = {
 export default function CreateTask({ closeTask }: Props) {
 	return (
 		<>
-			<div className=' absolute left-[50%] z-20 top-[158px] w-[1020px] h-[720px] bg-[#F9F9F9]'>
-				<h3>Add New Task</h3>
-				<button className='cursor-pointer' onClick={() => closeTask(false)}>
-					Go Back
-				</button>
+			<div className='absolute left-[50%] translate-x-[-50%] z-20 top-39.5 w-255 h-180 bg-[#F9F9F9] p-12.5'>
+				<div className='upperPart flex w-full justify-between'>
+					<div className='relative'>
+						<h3 className='addNewTaskHeading text-xl font-bold inline-block'>
+							Add New Task
+						</h3>
+					</div>
+					<button
+						className='cursor-pointer text-xl font-semibold underline'
+						onClick={() => closeTask(false)}
+					>
+						Go Back
+					</button>
+				</div>
 			</div>
 		</>
 	)
