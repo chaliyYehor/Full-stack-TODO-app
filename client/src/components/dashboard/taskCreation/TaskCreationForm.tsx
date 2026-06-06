e7a118f00fc1036ac3c35e09502e7bc690a4a11d
Yehor <chaliyegor00@gmail.com>
2026-06-06 16:24:14 +0300

    create task description component and styles for it

--- client/src/components/dashboard/taskCreation/TaskCreationForm.tsx
+++ client/src/components/dashboard/taskCreation/TaskCreationForm.tsx
@@ -3,6 +3,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import DatePickerInput from './DatePickerInput'
 import TextField from '@mui/material/TextField'
 import { useFormContext } from 'react-hook-form'
+import TextareaAutosize from '@mui/material/TextareaAutosize'
 import type { CreateTaskFormType } from '../../../schemas/createTaskFormSchema'
 
 export default function TaskCreationForm() {
@@ -28,6 +29,17 @@ export default function TaskCreationForm() {
 					<LocalizationProvider dateAdapter={AdapterDayjs}>
 						<DatePickerInput />
 					</LocalizationProvider>
+				</div>
+
+				<div className='description flex flex-col gap-2'>
+					<h3 className='font-semibold text-xl'>Description</h3>
+					<TextareaAutosize
+						placeholder='Start writing here...'
+						minRows={5}
+						maxRows={5}
+						{...register('taskDescription')}
+						className='w-full resize-none overflow-y-auto rounded border border-[#c4c4c4] bg-transparent px-3.5 py-4 text-base leading-6 outline outline-0 outline-offset-0 transition-colors placeholder:text-[#757575] hover:border-black focus:border-[#1976d2] focus:outline-1 focus:outline-[#1976d2]'
+					/>
 				</div>
 			</div>
 			<div className='secondColumn'></div>
