2aff66670dd843730451fcb194741a2cf018a0f4
Yehor <chaliyegor00@gmail.com>
2026-06-06 15:47:44 +0300

    create a component for date pick

---
+++ client/src/components/dashboard/taskCreation/DatePickerInput.tsx
@@ -0,0 +1,27 @@
+import { Controller, useFormContext } from 'react-hook-form'
+import type { CreateTaskFormType } from '../../../schemas/createTaskFormSchema'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+
+export default function DatePickerInput() {
+	const { control } = useFormContext<CreateTaskFormType>()
+
+	return (
+		<Controller
+			name='date'
+			control={control}
+			render={({ field, fieldState }) => (
+				<DatePicker
+					label='Due date'
+					value={field.value}
+					onChange={date => field.onChange(date)}
+					slotProps={{
+						textField: {
+							error: !!fieldState.error,
+							helperText: fieldState.error?.message,
+						},
+					}}
+				/>
+			)}
+		/>
+	)
+}
