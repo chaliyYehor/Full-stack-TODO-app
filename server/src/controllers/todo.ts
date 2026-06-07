b493a3fb4a07b2ac7cf267b5bbb0588bea19ac5c
Yehor <chaliyegor00@gmail.com>
2026-06-07 17:58:03 +0300

    handle task creation with cloudinary image storage

--- server/src/controllers/todo.ts
+++ server/src/controllers/todo.ts
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes'
 import multer from 'multer'
 import { createTaskSchema } from '../schemas/todoSchema.js'
 import { BadRequestError } from '../errors/bad-request.js'
+import Task from '../models/task.js'
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -15,10 +16,54 @@ export const upload = multer({ dest: 'uploads/' })
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024
 
-export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
+export const createTodo = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	const parsed = createTaskSchema.safeParse(req.body)
-	if(!parsed.success) {
+	if (!parsed.success) {
 		return next(new BadRequestError('Please provide proper credentials'))
 	}
-	res.status(200).send(req.body)
+
+	const { date, priority, title, taskDescription } = parsed.data
+
+	try {
+		let imageUrl = ''
+		let imagePublicId = ''
+
+		if (req.file) {
+			if (req.file.size > 5 * 1024 * 1024) {
+				return next(new BadRequestError('Max image size is 5MB'))
+			}
+
+			if (!req.file.mimetype.startsWith('image/')) {
+				return next(new BadRequestError('Only images are allowed'))
+			}
+
+			const result = await cloudinary.uploader.upload(req.file.path, {
+				folder: 'todo-tasks',
+			})
+			imageUrl = result.secure_url
+			imagePublicId = result.public_id
+		}
+
+		const task = {
+			title,
+			priority,
+			date,
+			taskDescription,
+			imageUrl,
+			imagePublicId,
+		}
+
+		const createTask = await Task.create(task)
+		if (!createTask) {
+			return next(new BadRequestError('Could not create a task'))
+		}
+
+		res.status(StatusCodes.CREATED).json(task)
+	} catch (error) {
+		return next(new BadRequestError('Image Upload failed'))
+	}
 }
