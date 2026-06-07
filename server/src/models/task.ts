071f77ba27a412f81a3ff9d003c2b1fb9351e9e3
Yehor <chaliyegor00@gmail.com>
2026-06-07 17:43:08 +0300

    create a model for task

---
+++ server/src/models/task.ts
@@ -0,0 +1,45 @@
+import mongoose, { HydratedDocument, Model } from 'mongoose'
+
+interface Task {
+	title: string
+	priority: string
+	date: string
+	taskDescription: string
+	imageUrl: string
+	imagePublicId: string
+}
+
+interface TaskMethods {}
+
+type TaskModel = Model<Task, {}, TaskMethods>
+
+export type TaskDoc = HydratedDocument<Task, TaskMethods>
+
+const TaskSchema = new mongoose.Schema<Task, TaskModel, TaskMethods>({
+	title: {
+		type: String,
+		required: [true, 'Please provide a Title'],
+		minLength: 2,
+		maxLength: 40,
+	},
+	priority: {
+		type: String,
+		enum: ['Extreme', 'Moderate', 'Low'],
+		required: true,
+	},
+	date: {
+		type: String,
+	},
+	taskDescription: {
+		type: String,
+		minLength: 5,
+	},
+	imageUrl: {
+		type: String,
+	},
+	imagePublicId: {
+		type: String,
+	},
+})
+
+export default mongoose.model<Task, TaskModel>('Task', TaskSchema)
