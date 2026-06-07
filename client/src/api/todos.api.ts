a205f277c496e20bd6b17e0b0ef2159f45c8a079
Yehor <chaliyegor00@gmail.com>
2026-06-07 18:20:24 +0300

    create an axios function that creates a task

--- client/src/api/todos.api.ts
+++ client/src/api/todos.api.ts
@@ -5,3 +5,11 @@ export const getAllTodos = async () => {
 
 	return response.data
 }
+
+export const createTodo = async (data: FormData) => {
+	const response = await api.post('/api/v1/todos/create', {
+		data,
+	})
+
+	return response.data
+}
