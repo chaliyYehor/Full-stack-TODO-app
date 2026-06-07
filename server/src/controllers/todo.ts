7eb4003f242288f21a625ce4cf845ad183bcbe87
Yehor <chaliyegor00@gmail.com>
2026-06-07 16:53:42 +0300

    create a schema that will check if the values from client for creating a task are correct

--- server/src/controllers/todo.ts
+++ server/src/controllers/todo.ts
@@ -5,11 +5,11 @@ import multer from 'multer'
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-export const upload = multer({dest: 'uploads/'})
+export const upload = multer({ dest: 'uploads/' })
 
-export const createTodo = async (req: Request, res: Response) => {
-	
-}
+const MAX_FILE_SIZE = 5 * 1024 * 1024
+
+export const createTodo = async (req: Request, res: Response) => {}
