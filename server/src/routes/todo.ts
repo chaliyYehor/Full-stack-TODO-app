0d22f36f21c98c0c703fbca06b63e2f5b6afc365
Yehor <chaliyegor00@gmail.com>
2026-06-08 18:05:31 +0300

    wire it up in the router

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createTodo, upload } from '../controllers/todo.js'
+import { createTodo, getAllTodos, upload } from '../controllers/todo.js'
 
 const router = express.Router()
 
 router.post('/create', upload.single('image'), createTodo)
+router.get('/', getAllTodos)
 
 export default router
