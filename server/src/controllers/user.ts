075c3ad24963689f5ea4298c792b669d1aa839bc
Yehor <chaliyegor00@gmail.com>
2026-06-08 17:24:13 +0300

    find a user by its id and send back user info

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,9 +1,24 @@
 import { NextFunction, Request, Response } from 'express'
+import User from '../models/user.js'
+import { BadRequestError } from '../errors/bad-request.js'
+import { StatusCodes } from 'http-status-codes'
 
 export const getUserInfo = async (
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ) => {
-	res.status(200).send(req.user?.userID)
+	const user = await User.findOne({ _id: req.user?.userID }).select([
+		'firstName',
+		'lastName',
+		'email',
+		'username',
+		'-_id',
+	])
+
+	if (!user) {
+		return next(new BadRequestError('No user was found'))
+	}
+
+	res.status(StatusCodes.OK).json(user)
 }
