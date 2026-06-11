e241df42ec207d1da5f02c0fbee24df7bb095dd4
Yehor <chaliyegor00@gmail.com>
2026-06-11 18:57:17 +0300

    add a rendering markup with real data and add go back btn

--- client/src/components/main/ViewTask.tsx
+++ client/src/components/main/ViewTask.tsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useGetAllTodos } from '../../hooks/useGetAllTodos'
 import type { TodoType } from '../../schemas/todosSchema'
+import dayjs from 'dayjs'
 
 export default function ViewTask() {
+	const navigate = useNavigate()
 	const { todoId } = useParams()
 	const { data: todos } = useGetAllTodos()
 	const todo = todos?.filter(todo => todo._id === todoId)
@@ -16,21 +18,66 @@ export default function ViewTask() {
 		imageUrl,
 		date,
 	} = todo[0]
+
+	const formattedDate = dayjs(createdAt).format('DD/MM/YYYY')
+
+	let statusColor =
+		status === 'Not Started'
+			? '#F21E1E'
+			: status === 'In Progress'
+				? '#0225FF'
+				: '#05A301'
+
+	const priorityColor =
+		priority === 'Extreme'
+			? '#F21E1E'
+			: priority === 'Moderate'
+				? '#3ABEFF'
+				: '#05A301'
+
 	return (
-		<div className='viewTaskWrapper'>
-			<div>
-				<div className='img'>
-					<img src={imageUrl} alt='img' />
+		<div className='viewTaskWrapper relative w-full h-[calc(100dvh-13rem)] mb-10 mt-15 ml-20 border-2 border-[#A1A3AB] rounded-2xl p-8'>
+			<button
+				type='button'
+				className='cursor-pointer text-xl font-semibold underline absolute right-10'
+				onClick={() => {
+					navigate(-1)
+				}}
+			>
+				Go Back
+			</button>
+			<div className='flex gap-5 mb-20'>
+				<div className='w-75 max-h-75 overflow-hidden'>
+					<img className='object-cover w-full' src={imageUrl} alt='img' />
 				</div>
-				<div className='info'>
-					<h2>{title}</h2>
-					<span>Priority: {priority}</span>
-					<span>Status: {status}</span>
-					<span>Created on: {createdAt}</span>
+				<div className='todo-info text-[13px] flex flex-col gap-2'>
+					<h2 className='font-bold text-4xl capitalize max-w-[80%] wrap-normal'>{title}</h2>
+					<div className='spanWrapper flex flex-col text-[20px]'>
+						<span>
+							Priority:{' '}
+							<span
+								className='font-semibold'
+								style={{
+									color: priorityColor,
+								}}
+							>
+								{priority}
+							</span>
+						</span>
+						<span>
+							Status:{' '}
+							<span className='font-semibold' style={{ color: statusColor }}>
+								{status}
+							</span>
+						</span>
+						<span className='text-[#A1A3AB]'>Created on: {formattedDate}</span>
+					</div>
 				</div>
 			</div>
 			<div>
-				<p>{taskDescription}</p>
+				<p className='text-[#747474] text-[18px] capitalize'>
+					{taskDescription}
+				</p>
 			</div>
 		</div>
 	)
