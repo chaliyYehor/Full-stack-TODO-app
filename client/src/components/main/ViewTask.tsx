287787c4336abea071d51c0c445aa0bf20118210
Yehor <chaliyegor00@gmail.com>
2026-06-11 18:34:43 +0300

    get todo data and render it

--- client/src/components/main/ViewTask.tsx
+++ client/src/components/main/ViewTask.tsx
@@ -1,8 +1,37 @@
 import { useParams } from 'react-router-dom'
+import { useGetAllTodos } from '../../hooks/useGetAllTodos'
+import type { TodoType } from '../../schemas/todosSchema'
 
 export default function ViewTask() {
-	const {todoId} = useParams()
+	const { todoId } = useParams()
+	const { data: todos } = useGetAllTodos()
+	const todo = todos?.filter(todo => todo._id === todoId)
+	if (!todo) return
+	const {
+		title,
+		priority,
+		status,
+		createdAt,
+		taskDescription,
+		imageUrl,
+		date,
+	} = todo[0]
 	return (
-		<div>{todoId}</div>
+		<div className='viewTaskWrapper'>
+			<div>
+				<div className='img'>
+					<img src={imageUrl} alt='img' />
+				</div>
+				<div className='info'>
+					<h2>{title}</h2>
+					<span>Priority: {priority}</span>
+					<span>Status: {status}</span>
+					<span>Created on: {createdAt}</span>
+				</div>
+			</div>
+			<div>
+				<p>{taskDescription}</p>
+			</div>
+		</div>
 	)
 }
